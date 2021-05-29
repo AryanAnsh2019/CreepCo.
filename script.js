f58dffd69361353ff5877532ada5d2fc20c43c91
AryanAnsh2019 <52231066+AryanAnsh2019@users.noreply.github.com>
2021-05-29 16:03:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      spaceBetween: 5,
+			effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+			navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+    });
+
